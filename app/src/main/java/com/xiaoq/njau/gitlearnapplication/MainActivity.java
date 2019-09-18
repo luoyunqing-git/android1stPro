9e79c60e1a2edb3f1f96a1c2d0297366c1274034
luoyunqing <luoyunqing2015@163.com>
2019-09-18 10:16:02 +0800

    First Commit

---
+++ app/src/main/java/com/xiaoq/njau/gitlearnapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.xiaoq.njau.gitlearnapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
